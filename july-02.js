11bf43ae66c23316b115d431ef056936751307e6
maninani2309 <maninani2309@gmail.com>
2024-07-03 15:52:05 +0530

    02-07-24

---
+++ july-02.js
@@ -0,0 +1,46 @@
+// Callback function
+
+// function greet(name, callback) {
+//     callback(name);
+//   }
+  
+//   function log(name) {
+//     console.log("hello,"+name);
+//   }
+  
+//   greet("Manikanta", log);
+
+// doubling an array using map method.
+
+// let arr = [1, 2, 3, 4, 5];
+// let doubledNumbers = doubleNumbers(arr);
+  
+// function doubleNumbers(arr) {
+//     return arr.map(num => num * 2);
+//   }
+//   console.log(doubledNumbers);
+
+
+// To Upper case 
+
+
+// let strings = ['hello', 'world', 'javascript'];
+// let upperCaseStrings = toUpperCaseArray(strings);
+ 
+// function toUpperCaseArray(str) {
+//     return str.map(string => string.toUpperCase());
+//   }
+//   console.log(upperCaseStrings);
+
+// applytoeach method
+
+// let arr = [1, 2, 3, 4, 5];
+// let doubledNumbers = applyToEach(arr, function(num) {
+//     return num * 2;
+//   });
+// function applyToEach(arr, callback) {
+//     return arr.map(callback);
+//   }
+  
+
+//   console.log(doubledNumbers);
