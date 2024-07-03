fa79b9a8158298cf5d57bb27ce908fa6e881e429
maninani2309 <maninani2309@gmail.com>
2024-07-03 16:26:41 +0530

    27-06-24

---
+++ june-27th.js
@@ -0,0 +1,25 @@
+// date of day
+
+// const d = new Date();
+// console.log(d)
+
+// day of the date
+
+// const date = new Date("2024-07-01");
+// const days = ['saturday','monday','tuesday','wednesday','thursday','friday','sunday'];
+// const dayOfWeek = days[date.getDay()]
+// console.log(dayOfWeek)
+
+// difference between the days
+
+// const date1 = new Date('2024-01-01');
+// const date2 = new Date('2024-07-01');
+// const oneDay = 24 * 60 * 60 * 1000; 
+// const date1Ms = date1.getTime();
+// const date2Ms = date2.getTime();
+// const differenceMs = Math.abs(date1Ms - date2Ms);
+// const days1 = Math.round(differenceMs / oneDay);
+// console.log(days1);
+// console.log(date1Ms);
+// console.log(date2Ms); 
+// console.log(differenceMs);
