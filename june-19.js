39039bddc62c714c8187db9f86639fb1f7717263
maninani2309 <maninani2309@gmail.com>
2024-07-03 16:18:41 +0530

    19-06-24

---
+++ june-19.js
@@ -0,0 +1,38 @@
+
+// reverse a string without using methods
+
+
+// let str = "hello";
+// let reverse = "";
+
+// for (let i = str.length - 1; i >= 0; i--) {
+//     reverse += str[i]; 
+// }
+
+// console.log(reverse);
+
+// printing ascii values before the letter:
+
+// function printAscii(string) {
+//     for (let i = 0; i < string.length; i++) {
+//       let char = string[i];
+//       let ascii = char.charCodeAt(0); 
+//       console.log(`ASCII of ${char} is ${ascii}`);
+//     }
+//   }
+//   printAscii("Manikanta");
+
+// let string = "Manikanta";
+// for (let i = 0; i < string.length; i++) {
+//     let char = string[i];
+//     let ascii = char.charCodeAt(0);
+
+//     console.log(`ASCII of ${char} is ${ascii}`)
+// }
+
+
+// remove the spaces between in a sentence:
+
+// let sentence = "Hello, World!";
+// let noSpaces = sentence.split(" ").join("");
+// console.log(noSpaces);
